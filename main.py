243d94b58e3944f99cc190e433950ca17114a640
Raphael Pattie <rpattiecsi@gmail.com>
2021-05-21 10:22:34 +0200

    created a welcome private message

--- main.py
+++ main.py
@@ -9,5 +9,11 @@ class MyClient(discord.Client):
     async def on_ready(self):
         print('Logged on as {0}!'.format(self.user))
 
+    # Event listener for a new member joining.
+    async def on_member_join(self, member):
+        guild = member.guild
+        member.send('Hello! And welcome to {guild.mention}.\nI am a bot, and will guide you through your first steps in this server.')
+
+
 client = MyClient()
 client.run(token)
